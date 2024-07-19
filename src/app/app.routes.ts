ca8733575b77a209c4a66d0a16d6578883232fe8
jonathanAndrade19 <jonathanjampamb@gmail.com>
2024-07-19 16:50:13 -0300

    Inicializando projeto, abertura de empresa.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { SolicitarAberturaEmpresaComponent } from './shared/component/solicitar-abertura-empresa/solicitar-abertura-empresa.component';
+import { EmpresaComponent } from './shared/component/empresa/empresa.component';
+export const routes: Routes = [
+    {
+        path: '',
+        component: EmpresaComponent,
+    },
+    {
+        path: 'empresa',
+        component: SolicitarAberturaEmpresaComponent,
+    },
+    {
+        path: 'empresa/:id',
+        component: SolicitarAberturaEmpresaComponent,
+    }
+];
